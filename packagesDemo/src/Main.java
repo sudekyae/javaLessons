a46a051fee494d93ba6354afbb2697449115640a
Sude <sudeky3834@gmail.com>
2026-08-06 21:06:56 +0300

    feat: add basic packages practice

---
+++ packagesDemo/src/Main.java
@@ -0,0 +1,21 @@
+//import matematik.DortIslem;
+//import matematik.Logaritma;
+import matematik. *; //matematik paketindeki her seyi kullan
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adınız: ");
+        String isim = scanner.nextLine();
+        System.out.println("Merhaba "+isim);
+
+        DortIslem dortIslem = new DortIslem();
+        dortIslem.topla(2,3);
+
+        Logaritma logaritma = new Logaritma();
+        logaritma.logaritmaHesapla();
+    }
+}
